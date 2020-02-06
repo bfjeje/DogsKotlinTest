179114cecba87f6bc808a68bd75c7bb80aebf28f
Bruno Dario Fernandez Ellerbach <ingbrunofernandez@gmail.com>
2020-02-06 15:03:50 -0300

    First commit

---
+++ app/src/main/java/bruno/fernandez/dogs/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package bruno.fernandez.dogs.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bruno.fernandez.dogs.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
