39ac65c3299ac84535145968b2a964afdbe2b91d
Bruno Dario Fernandez Ellerbach <ingbrunofernandez@gmail.com>
2020-02-06 23:34:06 -0300

    Finishing lecture of the New Navigation System in Android Studio.

--- app/src/main/java/bruno/fernandez/dogs/view/DetailFragment.kt
+++ app/src/main/java/bruno/fernandez/dogs/view/DetailFragment.kt
@@ -6,13 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 
 import bruno.fernandez.dogs.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class DetailFragment : Fragment() {
+
+    private var dogUuid = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +27,20 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //The ? means that arguments can be null
+        arguments?.let {
+            dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
+            textView2.text = dogUuid.toString()
+        }
+
+        buttonList.setOnClickListener {
+            val action: NavDirections = DetailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
 
 }
--- app/src/main/java/bruno/fernandez/dogs/view/ListFragment.kt
+++ app/src/main/java/bruno/fernandez/dogs/view/ListFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 
 import bruno.fernandez.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 /**
  * A simple [Fragment] subclass.
@@ -22,5 +25,15 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonDetails.setOnClickListener {
+            val action: ListFragmentDirections.ActionDetailFragment =
+                ListFragmentDirections.actionDetailFragment()
+            action.dogUuid = 5
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 
 }
