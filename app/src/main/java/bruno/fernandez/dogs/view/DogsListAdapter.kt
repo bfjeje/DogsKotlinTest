03113871ab0c052b2ffbe495f02ed91c6bbdd17c
Bruno Dario Fernandez Ellerbach <ingbrunofernandez@gmail.com>
2020-02-10 15:49:34 -0300

    Created the adapter for the dog list.

---
+++ app/src/main/java/bruno/fernandez/dogs/view/DogsListAdapter.kt
@@ -0,0 +1,38 @@
+package bruno.fernandez.dogs.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import bruno.fernandez.dogs.R
+import bruno.fernandez.dogs.model.DogBreed
+import kotlinx.android.synthetic.main.item_dog.view.*
+
+class DogsListAdapter(val dogsList: ArrayList<DogBreed>) :
+    RecyclerView.Adapter<DogsListAdapter.DogViewHolder>() {
+
+    //This is the Holder of all elements
+    class DogViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
+        val inflater: LayoutInflater = LayoutInflater.from(parent.context)
+        //The elements of the dogList will be transformed into views
+        val view = inflater.inflate(R.layout.item_dog, parent, false)
+        return DogViewHolder(view)
+    }
+
+    override fun getItemCount() = dogsList.size
+
+    //This links the element in the array with the Holder
+    override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
+        //I get the attributes of the holder from the layout
+        holder.view.name.text = dogsList[position].dogBreed
+        holder.view.lifespan.text = dogsList[position].lifespan
+    }
+
+    fun updateDogList(newDogsList: List<DogBreed>) {
+        dogsList.clear()
+        dogsList.addAll(newDogsList)
+        notifyDataSetChanged()
+    }
+}
